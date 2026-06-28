f7493475ad452b4c73363551872ecb6db002102b
Gustavo A. Batista A <182553806+GustavoBatistaAug@users.noreply.github.com>
2026-06-27 23:31:20 -0500

    [feat] Dependencias externas, api/v1,

--- src/app.js
+++ src/app.js
@@ -1,12 +1,69 @@
 import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import compression from "compression";
+import rateLimit from "express-rate-limit";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import errorMiddleware from './middlewares/error.middleware.js'
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
+const allowedOrigins = [
+    "http://localhost:5173"
+];
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100,
+    standardHeaders: "draft-8",
+    legacyHeaders: false,
+    message: {
+    success: false,
+    message: "Demasiadas solicitudes. Intente nuevamente más tarde."
+    }
+});
+
+app.use((req, res, next) => {
+    Object.defineProperty(req, 'query', {
+        value: { ...req.query },
+        writable: true,
+        configurable: true,
+        enumerable: true
+    });
+    next();
+});
+
+app.use(helmet());
+app.use(cors({
+        origin(origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+        }
+        callback(new Error("Origen no permitido por CORS."));
+        },
+    credentials: true
+    })
+);
+app.use(express.json({ limit: "10kb" }));
+app.use(mongoSanitize());
+app.use(hpp());
+app.use(compression());
+
+app.get("/api/v1", (req, res) => {
+    res.status(200).json({
         success: true,
-        message: "GarageSolutions API"
+        message: "GarageSolutions API v1"
     });
 });
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Recurso no encontrado."
+    });
+});
+
+app.use(errorMiddleware);
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import app from "./app.js";
 import "dotenv/config";
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}/api/v1`);
 });
