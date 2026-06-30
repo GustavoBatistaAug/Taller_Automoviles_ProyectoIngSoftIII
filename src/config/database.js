475cbfeebf5307bcba8477266620772c0d7698e8
Gustavo A. Batista A <182553806+GustavoBatistaAug@users.noreply.github.com>
2026-06-29 20:44:35 -0500

    [fix] Error de doble llamado de función

--- src/config/database.js
+++ src/config/database.js
@@ -4,10 +4,11 @@ export async function connectDatabase() {
     try {
         await mongoose.connect(process.env.MONGO_URI);
         console.log("MongoDB conectado correctamente.");
+        console.log("Database:", mongoose.connection.name);
     } catch (error) {
         console.error(error);
         process.exit(1);
     }
 }
 
-export default connectDatabase();
+export default connectDatabase;
